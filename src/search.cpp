1bfcac15333e67875579e878967dc92a969567df
czaky <andrzej.w@lczak.net>
2025-01-19 20:38:36 -0500

    improve maze

--- src/search.cpp
+++ src/search.cpp
@@ -31,34 +31,31 @@ int binary_search(const std::vector<int> &a, int k) {
 }
 
 std::vector<std::string> find_paths(const std::vector<std::vector<int>> &maze) {
-  int m = maze.size();
-  int n = maze[0].size();
+  const int m = maze.size();
+  const int n = maze[0].size();
 
   std::vector<char> path;
   std::vector<std::string> paths;
-  std::vector<bool> v(n * m, 0);
+  std::vector<std::vector<bool>> v(m, std::vector<bool>(n, 0));
 
   std::function<void(char, int, int)> dfs = [&](char d, int x, int y) {
-    if (x < 0 || y < 0 || x >= n || y >= m)
-      return;
-    const int vi = x + n * y;
-    if (maze[y][x] == 0 || v[vi])
+    if (x < 0 || y < 0 || x >= n || y >= m || maze[y][x] == 0 || v[y][x])
       return;
     path.push_back(d);
-    v[vi] = 1;
     if (x == n - 1 && y == m - 1) {
       paths.push_back(std::string(path.begin(), path.end()));
     } else {
+      v[y][x] = 1;
       dfs('U', x, y - 1);
       dfs('D', x, y + 1);
       dfs('L', x - 1, y);
       dfs('R', x + 1, y);
+      v[y][x] = 0;
     }
     path.pop_back();
-    v[vi] = 0;
   };
 
-  v[0] = 1;
+  v[0][0] = 1;
   dfs('D', 0, 1);
   dfs('R', 1, 0);
 
